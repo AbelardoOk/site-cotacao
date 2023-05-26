f50a66a254c99b1eeb7708ea4bdfc8304b31a40e
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-05-26 18:37:52 -0300

    pré criação front-end
    Criaçãp do container do meio e formulario, falta apenas alguns ajustes de alinhamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex items-center justify-center h-screen">
 
-      <div>
-        <h1>olá mundo</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit, omnis tempore dignissimos laborum necessitatibus.</p>
-      </div>
+      <div className="w-[670px] h-[355px] p-8 bg-black rounded-[40px]">
+        <h1 className="font-alt text-5xl">Cotação</h1>
       
+        <div className="mt-20">
+          {/* Parte cotação */}
+
+          <p className="text-2xl">1 Euro igual a</p>
+          <p className="text-2xl">5,35 Real Brasileiro</p>
+
+          <div>
+            <form>
+              <div>
+
+                <input className="w-[96px] h-[48px] bg-black rounded-l-[8px] text-center border-[#5AFB3A] border-l-2 border-t-2 border-b-2"
+                type="number" id='valor' placeholder="Valor" />
+
+                <select className="bg-black text-center w-[160px] h-[48px] border-2 border-[#5AFB3A]"
+                 name="deMoeda" id="deMoeda">
+
+                  <option value="">De</option>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="BRL">BRL</option>
+                </select>
+
+                <select className="bg-black rounded-r-[8px] text-center w-[160px] h-[48px] border-[#5AFB3A] border-r-2 border-t-2 border-b-2"
+                 name="paraMoeda" id="deMoeda">
+
+                  <option value="">De</option>
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="BRL">BRL</option>
+                </select>
+
+              </div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
     </main>
   )
 }
