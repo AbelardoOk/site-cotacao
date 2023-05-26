bf0de7d5683e77c371bf834a348abe176666f235
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-05-26 13:45:29 -0300

    Configuração inicial
    Aqui foram adicionadas as configurações do next.js no documento

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# site corretacao
+# site cotacao
  Um site criado utilizando react e tailwind Css juntamente de uma api para transformar os valores das moedas de acordo com a cotação
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+
+      <div>
+        <h1>olá mundo</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit, omnis tempore dignissimos laborum necessitatibus.</p>
+      </div>
+      
+    </main>
+  )
+}
